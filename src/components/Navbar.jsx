d0a0338c473d26646157a7c1e9a3d23eabf190c1
omprakashvadlapudi-OW <omprakash.vadlapudi@optimworks.com>
2025-11-30 18:38:31 +0530

    path change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import logo from '../public/assets/logo/2.png';
+import logo from '../../public/assets/logo/2.png';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const Navbar = () => {
